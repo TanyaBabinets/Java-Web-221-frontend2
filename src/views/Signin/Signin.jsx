c194c00235a9eb7640b513ae7a47a1a1fb109cf8
TanyaBabinets <tanya060616@gmail.com>
2025-03-27 15:22:40 +0200

    jwt_expired

--- src/views/Signin/Signin.jsx
+++ src/views/Signin/Signin.jsx
@@ -19,19 +19,46 @@ export default function Signin() {
       },
     }).then(data => {
       console.log("Ответ от сервера:", data);
-      if (data.user) {
+      if (data && data.user && data.jwtToken) {
         setUser(data.user);
         // setAccessToken(data.accessToken);
         setAccessToken(data.jwtToken);
+        console.log("JWT Token: ", data.jwtToken)
         navigate('/profile');
-      } else {
-        console.log("Ошибка на сервере:", data);
+        decodeJwt(data.jwtToken);
+      }else{
+        console.log("Ошибка на сервере, не пришел токен", data);
       }
     })
       .catch(error => {
         console.log("Ошибка при запросе:", error);
       });
   };
+  const decodeJwt = (token) => {
+    if (!token) { 
+      console.error("empty token");
+      return null;
+    }
+    try {
+      const base64Url = token.split('.')[1]; // берём часть токена payload
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const jsonPayload = decodeURIComponent(atob(base64).split('').map(c =>
+        '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+      ).join(''));
+
+      const payload = JSON.parse(jsonPayload);
+
+      console.log("Токен:", payload);
+      console.log("Issued at (iat):", new Date(payload.iat * 1000));
+      console.log("Expiration (exp):", new Date(payload.exp * 1000));
+
+      return payload;
+    } catch (error) {
+      console.error("Ошибка декодирования JWT:", error);
+      return null;
+    }
+  };
+ 
 
     return <form>
         <input 
